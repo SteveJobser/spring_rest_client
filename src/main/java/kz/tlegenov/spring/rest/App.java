c58c5828713e771901bb116c3bc1c9650e9ffd69
SteveJobser <tima_tleg@mail.ru>
2021-04-20 14:33:23 +0600

    Initial commit

---
+++ src/main/java/kz/tlegenov/spring/rest/App.java
@@ -0,0 +1,22 @@
+package kz.tlegenov.spring.rest;
+
+import kz.tlegenov.spring.rest.configuration.MyConfig;
+import kz.tlegenov.spring.rest.entity.Employee;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+
+        Communication communication = context.getBean("communication", Communication.class);
+        List<Employee> allEmployees = communication.getAllEmployees();
+        System.out.println(allEmployees);
+    }
+}
---
+++ src/main/java/kz/tlegenov/spring/rest/configuration/MyConfig.java
@@ -0,0 +1,16 @@
+package kz.tlegenov.spring.rest.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+@ComponentScan("kz.tlegenov.spring.rest")
+public class MyConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
